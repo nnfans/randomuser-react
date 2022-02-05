dae8df1e6da68b1943d343bcb91c77dee1329ddc
Mohamad Irfan <irfan.management@gmail.com>
2022-02-05 20:09:38 +0700

    feat: Add useFetch custom hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,58 @@
+import { useReducer } from 'react';
+import axios from 'axios';
+
+export const BASE_URL_API = 'https://randomuser.me/api/';
+
+const actionType = {
+  PENDING: 'PENDING',
+  RESOLVED: 'RESOLVED',
+  REJECTED: 'REJECTED',
+};
+
+export const statusType = { ...actionType, IDLE: 'IDLE' };
+
+const fetchReducer = (state, action) => {
+  switch (action.type) {
+    case actionType.PENDING: {
+      return { status: statusType.PENDING, data: null, error: null };
+    }
+    case actionType.RESOLVED: {
+      return { status: statusType.RESOLVED, data: action.data, error: null };
+    }
+    case actionType.REJECTED: {
+      return { status: statusType.REJECTED, data: null, error: action.error };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+};
+
+export const useFetch = ({
+  baseURL = BASE_URL_API,
+  method = 'GET',
+  url,
+} = {}) => {
+  if (!url) {
+    throw new Error("URL can't be empty");
+  }
+
+  const [{ status, data }, dispatch] = useReducer(fetchReducer, {
+    status: statusType.IDLE,
+    data: null,
+    error: null,
+  });
+
+  const fetch = async () => {
+    try {
+      dispatch({ type: actionType.PENDING });
+      const data = await axios.request({ method, baseURL, url });
+
+      dispatch({ type: actionType.RESOLVED, data });
+    } catch (error) {
+      dispatch({ type: actionType.REJECTED, error });
+    }
+  };
+
+  return { status, data, fetch };
+};
---
+++ src/hooks/useFetch.test.js
@@ -0,0 +1,91 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import axios from 'axios';
+
+import { useFetch, BASE_URL_API, statusType } from './useFetch';
+
+jest.mock('axios');
+
+describe('useFetch custom hook', () => {
+  it('throw an error when url is empty', async () => {
+    expect(useFetch).toThrowError(new Error("URL can't be empty"));
+  });
+
+  it('initial status is IDLE', async () => {
+    const { result } = renderHook(() => useFetch({ url: '/' }));
+
+    expect(result.current.status).toBe(statusType.IDLE);
+  });
+
+  it('data is equal to axios resolved value', async () => {
+    const { result } = renderHook(() => useFetch({ url: '/' }));
+
+    axios.request.mockResolvedValue('data');
+
+    await act(async () => {
+      await result.current.fetch();
+    });
+
+    expect(result.current.data).toBe('data');
+  });
+
+  it('status is PENDING then RESOLVED', async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetch({ url: '/' })
+    );
+
+    axios.request.mockImplementation(
+      () => new Promise((resolve) => setTimeout(resolve, 1000))
+    );
+
+    await act(async () => {
+      const fetchPromise = result.current.fetch();
+      await waitForNextUpdate();
+      expect(result.current.status).toBe(statusType.PENDING);
+
+      await fetchPromise;
+      expect(result.current.status).toBe(statusType.RESOLVED);
+    });
+  });
+
+  it('status is REJECTED as axios request error', async () => {
+    const { result } = renderHook(() => useFetch({ url: '/' }));
+
+    axios.request.mockImplementation(() => {
+      throw new Error('Error');
+    });
+
+    await act(async () => {
+      await result.current.fetch();
+    });
+
+    expect(result.current.status).toBe(statusType.REJECTED);
+  });
+
+  it('call axios.request with some default parameter', async () => {
+    const { result } = renderHook(() => useFetch({ url: '/' }));
+
+    await act(async () => {
+      await result.current.fetch();
+    });
+
+    expect(axios.request).toHaveBeenCalledWith({
+      baseURL: BASE_URL_API,
+      method: 'GET',
+      url: '/',
+    });
+  });
+
+  it('call axios.request with correct parameter', async () => {
+    const { result } = renderHook(() =>
+      useFetch({ baseURL: 'baseURL', method: 'METHOD', url: '/' })
+    );
+
+    await act(result.current.fetch);
+
+    expect(axios.request).toHaveBeenCalledWith({
+      baseURL: 'baseURL',
+      method: 'METHOD',
+      url: '/',
+    });
+  });
+});
