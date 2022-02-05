73f848aa2faaa91f50d2ee755c62fe09125f39ee
Mohamad Irfan <irfan.management@gmail.com>
2022-02-05 20:11:37 +0700

    test(useFetch): Wrap fetch() test

--- src/hooks/useFetch.test.js
+++ src/hooks/useFetch.test.js
@@ -16,76 +16,78 @@ describe('useFetch custom hook', () => {
     expect(result.current.status).toBe(statusType.IDLE);
   });
 
-  it('data is equal to axios resolved value', async () => {
-    const { result } = renderHook(() => useFetch({ url: '/' }));
+  describe('fetch()', () => {
+    it('data is equal to axios resolved value', async () => {
+      const { result } = renderHook(() => useFetch({ url: '/' }));
 
-    axios.request.mockResolvedValue('data');
+      axios.request.mockResolvedValue('data');
 
-    await act(async () => {
-      await result.current.fetch();
+      await act(async () => {
+        await result.current.fetch();
+      });
+
+      expect(result.current.data).toBe('data');
     });
 
-    expect(result.current.data).toBe('data');
-  });
+    it('status is PENDING then RESOLVED', async () => {
+      const { result, waitForNextUpdate } = renderHook(() =>
+        useFetch({ url: '/' })
+      );
 
-  it('status is PENDING then RESOLVED', async () => {
-    const { result, waitForNextUpdate } = renderHook(() =>
-      useFetch({ url: '/' })
-    );
+      axios.request.mockImplementation(
+        () => new Promise((resolve) => setTimeout(resolve, 1000))
+      );
 
-    axios.request.mockImplementation(
-      () => new Promise((resolve) => setTimeout(resolve, 1000))
-    );
+      await act(async () => {
+        const fetchPromise = result.current.fetch();
+        await waitForNextUpdate();
+        expect(result.current.status).toBe(statusType.PENDING);
 
-    await act(async () => {
-      const fetchPromise = result.current.fetch();
-      await waitForNextUpdate();
-      expect(result.current.status).toBe(statusType.PENDING);
-
-      await fetchPromise;
-      expect(result.current.status).toBe(statusType.RESOLVED);
-    });
-  });
-
-  it('status is REJECTED as axios request error', async () => {
-    const { result } = renderHook(() => useFetch({ url: '/' }));
-
-    axios.request.mockImplementation(() => {
-      throw new Error('Error');
+        await fetchPromise;
+        expect(result.current.status).toBe(statusType.RESOLVED);
+      });
     });
 
-    await act(async () => {
-      await result.current.fetch();
+    it('status is REJECTED as axios request error', async () => {
+      const { result } = renderHook(() => useFetch({ url: '/' }));
+
+      axios.request.mockImplementation(() => {
+        throw new Error('Error');
+      });
+
+      await act(async () => {
+        await result.current.fetch();
+      });
+
+      expect(result.current.status).toBe(statusType.REJECTED);
     });
 
-    expect(result.current.status).toBe(statusType.REJECTED);
-  });
+    it('call axios.request with some default parameter', async () => {
+      const { result } = renderHook(() => useFetch({ url: '/' }));
 
-  it('call axios.request with some default parameter', async () => {
-    const { result } = renderHook(() => useFetch({ url: '/' }));
+      await act(async () => {
+        await result.current.fetch();
+      });
 
-    await act(async () => {
-      await result.current.fetch();
+      expect(axios.request).toHaveBeenCalledWith({
+        baseURL: BASE_URL_API,
+        method: 'GET',
+        url: '/',
+      });
     });
 
-    expect(axios.request).toHaveBeenCalledWith({
-      baseURL: BASE_URL_API,
-      method: 'GET',
-      url: '/',
-    });
-  });
+    it('call axios.request with correct parameter', async () => {
+      const { result } = renderHook(() =>
+        useFetch({ baseURL: 'baseURL', method: 'METHOD', url: '/' })
+      );
 
-  it('call axios.request with correct parameter', async () => {
-    const { result } = renderHook(() =>
-      useFetch({ baseURL: 'baseURL', method: 'METHOD', url: '/' })
-    );
+      await act(result.current.fetch);
 
-    await act(result.current.fetch);
-
-    expect(axios.request).toHaveBeenCalledWith({
-      baseURL: 'baseURL',
-      method: 'METHOD',
-      url: '/',
+      expect(axios.request).toHaveBeenCalledWith({
+        baseURL: 'baseURL',
+        method: 'METHOD',
+        url: '/',
+      });
     });
   });
 });
